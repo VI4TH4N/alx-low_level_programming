5c51e4f28cb1b09fd0b4af40c3b16db69aa79d76
VI4THAN <aiden.mabhula18@gmail.com>
2023-04-12 15:12:16 +0200

    Update

--- 0x09-static_libraries/2-strncpy.c
+++ 0x09-static_libraries/2-strncpy.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _strncpy - copy a string
  * @dest: input value
@@ -8,7 +7,6 @@
  *
  * Return: dest
  */
-
 char *_strncpy(char *dest, char *src, int n)
 {
         int j;
