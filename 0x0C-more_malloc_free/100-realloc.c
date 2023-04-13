43f47dab38d5cf1cb11b1991a466841e2138e9a7
VI4THAN <aiden.mabhula18@gmail.com>
2023-04-13 19:36:14 +0200

    C - More malloc, free

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -23,7 +23,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
                 return (NULL);
         }
         if (!ptr)
-                return (malloc(new_size));u
+                return (malloc(new_size));
         ptr1 = malloc(new_size);
         if (!ptr1)
                 return (NULL);
